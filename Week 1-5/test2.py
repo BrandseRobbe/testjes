115c8f069acc73deafa0a8bd6eb7c5ec35f9d022
RobbeBrandse <robbe.brandse@student.howest.be>
2019-01-20 23:40:56 +0100

    examen1

--- Week 1-5/test2.py
+++ Week 1-5/test2.py
@@ -7,10 +7,25 @@
 #     bestand.close()
 # print_inhoud("nmct.txt")
 
-setvb = {5,5,4,5,6,4,9}
-setvb2 = {5,5,4,5,6,4,8}
-setvb = set(list(setvb) + list(setvb2))
-setvb =sorted(setvb)
-print(setvb)
+setvb = {5, 5, 4, 5, 6, 4, 9}
 print(type(setvb))
-print(set(setvb))
+setvb2 = {5, 5, 4, 5, 6, 4, 8}
+print(setvb2)
+setvb = set(list(setvb) + list(setvb2))
+
+print(setvb)
+setvb = set(setvb)
+
+print(type(setvb))
+
+
+def test(waarde="0123456789"):
+    waarde += ""
+    return waarde
+
+
+test1 = test()
+test1 = list(test1)
+print(test1)
+print(test1.index("7"))
+print(len(test1))
